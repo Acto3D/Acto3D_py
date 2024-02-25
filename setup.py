5c93ed0537ae5cf949fca042fc04ef905be16bfd
Acto3D <n.takeshita.x+acto3d@gmail.com>
2024-02-25 15:10:04 +0900

    Requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='Acto3D', 
     version="0.1", 
-    install_requires=["numpy", "time", "numpy", "socket", "struct", "tqdm"],
+    install_requires=["numpy", "socket", "struct", "tqdm"],
     description="Preview ndarray in 3D with Acto3D (MacOS App)", 
     author='Naoki Takeshita', 
     packages=find_packages(),  
