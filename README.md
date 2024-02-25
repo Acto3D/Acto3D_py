16bb5a4fc27f700ec27c2f6c3c0fc843df766136
Acto3D <n.takeshita.x+acto3d@gmail.com>
2024-02-25 14:39:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Acto3D_py
+Python package for napari image layer (ndarray data) transfer to Acto3D (MacOS app).
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Acto3D', 
+    version="0.1", 
+    install_requires=["numpy", "subprocess", "time", "numpy", "socket", "struct", "tqdm"],
+    description="Preview ndarray in 3D with Acto3D (MacOS App)", 
+    author='Naoki Takeshita', 
+    packages=find_packages(),  
+    license='MIT'  
+)
