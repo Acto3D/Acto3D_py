52e68d1e4426638df459d528b1266f15c000ff4a
Acto3D <n.takeshita.x+acto3d@gmail.com>
2024-02-25 14:58:17 +0900

    不要なファイルを削除

--- Acto3D/__init__.py
+++
@@ -1,6 +0,0 @@
-# /Acto3D/__init__.py
-from .config import config
-from .main import openActo3D, check_layers_structure, transfer
-
-# オプショナル: パッケージをインポートした際に表示するメッセージ（デバッグや情報提供用）
-print("Acto3D has been imported.")
--- README.md
+++
@@ -1,2 +0,0 @@
-# Acto3D_py
-Python package for napari image layer (ndarray data) transfer to Acto3D (MacOS app).
