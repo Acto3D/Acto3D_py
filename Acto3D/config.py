fcd20231349fe6ded1774f726d6a06b1fe1ad24f
Acto3D <n.takeshita.x+acto3d@gmail.com>
2024-03-07 00:27:15 +0900

    import library error fixed

--- Acto3D/config.py
+++ Acto3D/config.py
@@ -2,7 +2,7 @@
 class Config:
     def __init__(self):
         self.host = 'localhost'
-        self.port = 54847
+        self.port = 41233
         self.path_to_Acto3D = '/Applications/Acto3D.app'
         
     def print_params(self):
